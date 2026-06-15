c83da09a6ede4a6ad232d969c03ed04771c40c0d
gedeonnkishi <gedeon.nkishi@unikin.ac.cd>
2026-06-15 04:39:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+
+# Sécurité si le README est manquant lors de certains tests locaux rapides
+long_description = ""
+if os.path.exists("README.md"):
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
 
 setup(
     name="mq_cenn",
     version="1.0.0",
-    py_modules=["mq_cenn"],
+    py_modules=["mq_cenn"], # Cherche précisément mq_cenn.py à la racine
     install_requires=[
         "numpy>=1.22.0",
         "torch>=2.0.0",
@@ -11,7 +18,7 @@ setup(
     ],
     author="Gédéon Nkishi",
     description="Multi-Quantum Cellular Neural Network (MQ-CeNN) for Time Series Forecasting",
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gedeonnkishi/mq-cenn",
     classifiers=[
