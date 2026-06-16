6ba087d0e210869028dfef153a71425693b9d12f
gedeonnkishi <gedeon.nkishi@unikin.ac.cd>
2026-06-16 14:46:04 +0100

    Update __init__.py

--- mq_cenn/__init__.py
+++ mq_cenn/__init__.py
@@ -1,1 +1,29 @@
+"""
+MQ-CeNN public API.
 
+This package currently re-exports the legacy single-file implementation.
+The code will be migrated module by module.
+"""
+
+from .legacy import (
+    KernelSpec,
+    DEFAULT_KERNEL_SPECS,
+    MQCeNNRegressor,
+    MQCeNNTrace,
+    make_ablation_suite,
+    set_global_seed,
+)
+
+try:
+    from .legacy import __version__ as __version__
+except Exception:
+    __version__ = "0.1.0"
+
+__all__ = [
+    "KernelSpec",
+    "DEFAULT_KERNEL_SPECS",
+    "MQCeNNRegressor",
+    "MQCeNNTrace",
+    "make_ablation_suite",
+    "set_global_seed",
+]
