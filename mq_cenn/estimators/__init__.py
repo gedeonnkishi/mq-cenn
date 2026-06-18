7a772219311717c8f57bfdc6a1d89e36b2de3d9d
gedeonnkishi <gedeon.nkishi@unikin.ac.cd>
2026-06-18 01:00:57 +0100

    Update __init__.py

--- mq_cenn/estimators/__init__.py
+++ mq_cenn/estimators/__init__.py
@@ -1,8 +1,14 @@
 from .regressor import FallbackStrategy, MQCeNNRegressor, MQCeNNTrace
+from .multistep import MQCeNNMultiStepRegressor, MQCeNNMultiStepTrace
+from .anomaly import MQCeNNAnomalyDetector, MQCeNNAnomalyTrace
 
 
 __all__ = [
     "FallbackStrategy",
     "MQCeNNRegressor",
     "MQCeNNTrace",
+    "MQCeNNMultiStepRegressor",
+    "MQCeNNMultiStepTrace",
+    "MQCeNNAnomalyDetector",
+    "MQCeNNAnomalyTrace",
 ]
